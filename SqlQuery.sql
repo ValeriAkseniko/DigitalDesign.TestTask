b4b9fc95cfa41d03c559b7b7a18f1e59188f55ad
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-09-20 16:37:41 +0300

    Add project files.

---
+++ SqlQuery.sql
@@ -0,0 +1,60 @@
+﻿--1.Сотрудника с максимальной заработной платой.
+-- В данном случае затрачивается ресорс на сортировку.
+SELECT TOP(1) 
+   Id AS EmployeeID,
+   Name AS EmployeeName,
+   Salary
+FROM Employee
+ORDER BY Salary DESC
+
+-- Здесь же затраты уходят на подзапрос.
+SELECT TOP(1) 
+   Id AS EmployeeID,
+   Name AS EmployeeName,
+   Salary
+FROM Employee
+WHERE Salary = (SELECT MAX(Salary) FROM Employee)
+
+
+--2.Отдел, с самой высокой заработной платой между сотрудниками. 
+SELECT TOP(1) 
+  Department.Id AS DepartmentId, 
+  Department.Name AS DepartmentName,
+  Employee.Salary AS MaxSalary
+FROM Department
+JOIN Employee ON Department.Id = Employee.DepartmentId
+ORDER BY Employee.Salary DESC
+
+
+--3.Отдел, с максимальной суммарной зарплатой сотрудников. 
+SELECT TOP(1) 
+   Sum(Salary) AS SumSalary,
+   Department.Id AS DepartmentId, 
+   Department.Name AS DepartmentName
+FROM Employee
+JOIN Department ON Employee.DepartmentId = Department.Id
+GROUP BY Department.Id , Department.Name
+ORDER BY SumSalary DESC
+
+--4.
+--Сотрудника, чье имя начинается на «Р» и заканчивается на «н».
+SELECT TOP(1) 
+   Employee.Id AS EmployeeId,
+   Employee.Name AS EmployeeName
+FROM Employee
+WHERE Employee.Name LIKE 'Р%н'
+
+--Имя начинается на «Р».
+SELECT TOP(1) 
+   Employee.Id AS EmployeeId,
+   Employee.Name AS EmployeeName
+FROM Employee
+WHERE Employee.Name LIKE 'Р_'
+
+--Имя заканчивается на «н».
+SELECT TOP(1) 
+   Employee.Id AS EmployeeId,
+   Employee.Name AS EmployeeName
+FROM Employee
+WHERE Employee.Name LIKE '%н'
+
